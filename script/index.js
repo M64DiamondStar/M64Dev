4f9b30e5f64868ba6d37b833f029690ae73bdb2d
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2025-12-24 10:01:30 +0100

    Added link to status

--- script/index.js
+++ script/index.js
@@ -1,6 +1,10 @@
+const outputElement = document.getElementById("online-status");
+const tooltipElement = document.getElementById("online-tooltip");
+
+document.addEventListener("mousemove", changeTooltipLocation);
+
 window.onload = function () {
   const apiUrl = `https://status-api.m64.dev/status/get`;
-  const outputElement = document.getElementById("online-status");
 
   fetch(apiUrl)
     .then((response) => {
@@ -41,3 +45,8 @@ window.onload = function () {
     })
     .catch((error) => console.error("Error fetching status:", error));
 };
+
+function changeTooltipLocation(e) {
+  tooltipElement.style.left = e.pageX - tooltipElement.offsetWidth / 2 + "px";
+  tooltipElement.style.top = e.pageY - tooltipElement.offsetHeight * 1.2 + "px";
+}
